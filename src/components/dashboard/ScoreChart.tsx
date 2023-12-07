3bd26a1eda4656e18ece7e31acb3f4f37e11d802
mamadykonte <contactmamadykonte@gmail.com>
2023-12-07 17:17:37 +0100

    format score && remove ErrorBoundary

--- src/components/dashboard/ScoreChart.tsx
+++ src/components/dashboard/ScoreChart.tsx
@@ -12,6 +12,8 @@ export const ScoreChart: FC<ScoreChartProps> = ({ score }) => {
     return null;
   }
 
+  const formatScore = Math.floor(score * 100);
+
   return (
     <Card className="relative bg-primary">
       <p className="absolute top-6 left-8 text-[#20253A] text-base font-bold">
@@ -21,11 +23,11 @@ export const ScoreChart: FC<ScoreChartProps> = ({ score }) => {
       <ResponsiveContainer>
         <PieChart>
           <Pie
-            data={[{ name: "Score", value: score * 100 }]}
+            data={[{ name: "Score", value: Math.floor(formatScore) }]}
             cx="50%"
             cy="50%"
             startAngle={90}
-            endAngle={90 + score * 100 * (360 / 100)}
+            endAngle={90 + formatScore * (360 / 100)}
             innerRadius={"60%"}
             outerRadius={"70%"}
             dataKey="value"
@@ -40,7 +42,7 @@ export const ScoreChart: FC<ScoreChartProps> = ({ score }) => {
       <div className="bg-white w-[150px] h-[150px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full flex flex-col justify-center items-center">
         <p className="text-base font-semibold text-[#74798C] flex flex-col justify-center">
           <span className="text-2xl font-bold text-[#282D30]">
-            {score * 100}%
+            {Math.floor(formatScore)}%
           </span>
           <span> de votre</span>
           <span> objectif</span>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,8 @@
-import {createContext } from "react";
+import { createContext } from "react";
 import { useParams } from "react-router-dom";
 import useFetchData from "../hooks/useFetchData";
 import { getUser } from "../service/api/user";
 
-import ErrorBoundary from "../components/sharing/ErrorBoundary";
 import { UserInfo } from "../components/dashboard/UserInfo";
 import { NavBar } from "../components/layout/NavBar";
 import { SideBar } from "../components/layout/SideBar";
@@ -30,39 +29,38 @@ const Dashboard = () => {
       <main>
         <NavBar />
         <SideBar />
-        <ErrorBoundary fallback={<div>Failed to fetch data!</div>}>
-          <div className="p-4 mt-24 ml-28 lg:py-16 lg:pl-28 lg:pr-20">
-            {isLoading && <div className="">Loading... User Info </div>}
-            {error || !userInfos ? (
+
+        <div className="p-4 mt-24 ml-28 lg:py-16 lg:pl-28 lg:pr-20">
+          {isLoading && <div className="">Loading... User Info </div>}
+          {error || !userInfos ? (
+            <div>Failed to fetch data!</div>
+          ) : (
+            <UserInfo userInfos={userInfos} />
+          )}
+
+          <div className="flex flex-col-reverse lg:flex-row flex-wrap gap-8">
+            <div className="w-full flex flex-wrap lg:w-3/5 flex-grow gap-12 lg:gap-7">
+              <ActivityBarChart />
+
+              <div className="flex flex-wrap flex-grow justify-between gap-7">
+                <AverageSessionChart />
+
+                <PerformanceChart />
+
+                {error || !score ? (
+                  <div>Failed to fetch data!</div>
+                ) : (
+                  <ScoreChart score={score} />
+                )}
+              </div>
+            </div>
+            {error || !nutritionData ? (
               <div>Failed to fetch data!</div>
             ) : (
-              <UserInfo userInfos={userInfos} />
+              <Nutrition nutritionData={nutritionData} />
             )}
-
-            <div className="flex flex-col-reverse lg:flex-row flex-wrap gap-8">
-              <div className="w-full flex flex-wrap lg:w-3/5 flex-grow gap-12 lg:gap-7">
-                <ActivityBarChart />
-
-                <div className="flex flex-wrap flex-grow justify-between gap-7">
-                  <AverageSessionChart />
-
-                  <PerformanceChart />
-
-                  {error || !score ? (
-                    <div>Failed to fetch data!</div>
-                  ) : (
-                    <ScoreChart score={score} />
-                  )}
-                </div>
-              </div>
-              {error || !nutritionData ? (
-                <div>Failed to fetch data!</div>
-              ) : (
-                <Nutrition nutritionData={nutritionData} />
-              )}
-            </div>
           </div>
-        </ErrorBoundary>
+        </div>
       </main>
     </UserIdContext.Provider>
   );
